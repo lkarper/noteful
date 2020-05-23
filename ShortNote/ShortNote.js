01e63a4d0acce66e3afec233e34a8d212b014f1e
Luther Karper <luther.karper@live.com>
2020-05-23 15:07:56 -0400

    Implement default props and PropType typechecking

--- ShortNote/ShortNote.js
+++ ShortNote/ShortNote.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import NotesContext from '../NotesContext';
 import './ShortNote.css';
 
@@ -10,6 +11,16 @@ class ShortNote extends Component {
     }
 
     static contextType = NotesContext;
+
+    static defaultProps = {
+        note: {
+            name: "Placeholder name",
+            id: "PlaceholderId", 
+            modified: (new Date()).toJSON(), 
+            folderId: "PlaceholderFolderId", 
+            content: "Lorem ipsum...",
+        }
+    }
 
     deleteNoteRequest = (noteId, cb) => {
         fetch(`http://localhost:9090/notes/${noteId}`, {
@@ -44,7 +55,7 @@ class ShortNote extends Component {
 
         const { note } = this.props;
         const date = new Date(note.modified);
-        
+
         return (
             <li>
                 <Link to={`/note/${note.id}`}>
@@ -59,6 +70,16 @@ class ShortNote extends Component {
             </li>
         );
     }
+}
+
+ShortNote.propTypes = {
+    note: PropTypes.shape({
+        name: PropTypes.string,
+        id: PropTypes.string,
+        modified: PropTypes.string,
+        folderId: PropTypes.string,
+        content: PropTypes.string,
+    }).isRequired,
 }
 
 export default ShortNote;
